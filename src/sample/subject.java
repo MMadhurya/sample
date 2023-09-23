536e1683677ef6ecf677d3ba2910e5931fd7b7b9
MMadhurya <m.madhurya26@gmail.com>
2023-09-23 15:06:50 +0530

    learn to push

--- src/sample/subject.java
+++ src/sample/subject.java
@@ -5,6 +5,7 @@ public class subject {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("this is to learn github");
+		//this is to push to githubs
 	}
 
 }
